bf4dfd8145ad545532eaea2a97bea54b9cc0f66e
skylon2k2 <skylon2k2@gmail.com>
2022-05-30 19:48:13 -0400

    Completed Heap sort

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## Algorithms Included
 - [Bubble Sort](./bubbleSort.py)
 - [Insertion Sort](./insertionSort.py)
+- [Heap Sort](./heapSort.py)
 - [Selection Sort](./selectionSort.py)
 
 ## Incomplete Algorithms
 - [Quick Sort](./quickSort.py)
 - [Merge Sort](./mergeSort.py)
-- [Heap Sort](./heapSort.py) (WIP, works half the time)
--- heapSort.py
+++ heapSort.py
@@ -6,28 +6,6 @@ Terminology
         A heap/tree where every child has a value less than or 
         equal to its parent. The largest item is at the root.
 
-                       217, 
-        375,                           784, 
-   287,         172,             557,         948, 
-986, 661,   410
-
-
-           0
-    1             2
- 3     4       5     6
-7 8   9
-
-          12
-  9       10      11
-0 1 2   3 4 5   6 7 8
-
-          1
-  |       |        |
-  2       3        4
-| | |   | | |   |  |  |
-5 6 7   8 9 10  11 12 13
-
-
 Run this in the terminal:
     ./heapSort.py
 '''
@@ -49,12 +27,11 @@ def sort(L: List[int]) -> None:
 
     '''
     buildMaxHeap(L)
-    printTree(L)
     lastItem = len(L)-1
     while lastItem > 0:
         L[0], L[lastItem] = L[lastItem], L[0]
         lastItem -= 1
-        heapify(L, lastItem+1)
+        heapify(L, 0, lastItem)
 
 
 def printTree(L):
@@ -65,7 +42,7 @@ def printTree(L):
     print("\n")
 
 
-def heapify(L: List[int], L_length: int) -> None:
+def heapify(L: List[int], start: int, end: int) -> None:
     """
     Creates a max heap from an partly sorted array
 
@@ -73,11 +50,11 @@ def heapify(L: List[int], L_length: int) -> None:
     bottom of the tree, by repeatedly swapping with the largest child to 
     ensure that the resulting heap is a max heap.
     """
-    currIndex = 0
-    while (2*currIndex + 1) < L_length:       # There is at least one leaf
+    currIndex = start
+    while (2*currIndex + 1) < end + 1:       # There is at least one leaf
         leftChildIndex = 2*currIndex + 1
         rightChildIndex = 2*currIndex + 2
-        if rightChildIndex < L_length:
+        if rightChildIndex < end + 1:
             if max(L[2*currIndex + 1], L[2*currIndex + 2]) > L[currIndex]:       # There are two leaves (easier way is to check parity of len(L), but this is more readable)
                 if L[leftChildIndex] >= L[rightChildIndex]:
                     L[currIndex], L[leftChildIndex] = L[leftChildIndex], L[currIndex]
@@ -96,6 +73,7 @@ def heapify(L: List[int], L_length: int) -> None:
 
 
 def buildMaxHeap(L: List[int]) -> None:
+    # Note buildMaxHeap should use a similar implementation but we heapify the last parent, and move backwards
     """
     Creates a max heap from an unsorted array
 
@@ -104,24 +82,9 @@ def buildMaxHeap(L: List[int]) -> None:
     heap, we check whether the max of left and right child is greater than 
     this parent. If it is, then we switch the parent with the larger child.
     """
-    if len(L) < 2:
-        return None
-    currIndex = floor(len(L)/2)-1        # Index of current parent, initialized to final parent
-    # If len(L) is even, the last parent has only one child
-    if len(L)%2 == 0:       # One child for last parent
-        if L[len(L)-1] > L[currIndex]:
-            L[len(L)-1], L[currIndex] = L[currIndex], L[len(L)-1]
-        currIndex -= 1
-    print("after len(L)%2==0\n")
-    printTree(L)
-    while currIndex >= 0:
-        print("\t", currIndex)
-        if max(L[(2*currIndex) + 1], L[(2*currIndex) + 2]) > L[currIndex]:
-            if L[2*currIndex + 1] > L[2*currIndex + 2]:
-                L[2*currIndex + 1], L[currIndex] = L[currIndex], L[2*currIndex + 1]
-            else:
-                L[2*currIndex + 2], L[currIndex] = L[currIndex], L[2*currIndex + 2]
-        currIndex -= 1
+
+    for i in range(floor(len(L)/2)):
+        heapify(L, floor(len(L)/2)-1-i, len(L)-1)
 
 
 def isNonDescending(L: List[int]) -> bool:
@@ -146,7 +109,7 @@ if __name__ == '__main__':
     
     print("\tTESTING...\nCreating array...")
     L = []
-    for _ in range(7):
+    for _ in range(100):
         L.append(random.randint(0, 1000))
     print("The unsorted array is", end=" ")
     print(L)    
